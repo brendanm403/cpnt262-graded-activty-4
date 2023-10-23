db33d0658b7104dd5bb1a1b6ca72412ff9fe4417
Brendan Malycky <bmalycky@hotmail.com>
2023-10-23 13:59:27 -0600

    added function to capitalize words

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
+let targetedElement;
+
 const reverseString = function(input) {
-  // select element to render in //
-  let targetedElement = document.getElementById("reversed-string");
   // get each individual letter into an array //
   let lettersArray = input.split("");
   // only accept input longer than 1 character //
@@ -16,6 +16,8 @@ const reverseString = function(input) {
     }
     // remove commas to its a single string again//
     let finalResult = reversedArray.join("");
+    // select element to render in //
+    targetedElement = document.getElementById("reversed-string");
     // render final value in html //
     targetedElement.innerHTML = finalResult;
     // handles input that is less than 2 characters //
@@ -24,10 +26,7 @@ const reverseString = function(input) {
   }
 }
 
-reverseString("abra kadabra alakazam");
-
 const myArray = ["hello", 0, 5, 14, "", false, true];
-
 
 // This is the version that MUTATES THE ORIGINAL ARRAY //
 const removeFalsy = function (inputArr) {
@@ -45,8 +44,6 @@ const removeFalsy = function (inputArr) {
 
 // this is the version that DOES NOT MUTATE THE ORIGINAL ARRAY //
 const removeFalsyWithoutMutating = function (inputArr) {
-  // select element to render in //
-  let targetedElement = document.getElementById("remove-falsy-original");
   // create clone of original array //
   const clonedArray = inputArr.map( x => x);
   // use map on the clone to avoid mutating original array //
@@ -55,13 +52,44 @@ const removeFalsyWithoutMutating = function (inputArr) {
     if (!element) {
       // remove falsy values at its index, insert spliced //
       clonedArray.splice(index, 1, "spliced");
+      // select element to render in //
+      targetedElement = document.getElementById("remove-falsy-original");
       // render original array //
       targetedElement.innerHTML = myArray;
-      // render cloned array //
+      // select element to render in //
       targetedElement = document.getElementById("remove-falsy-clone");
+      // render cloned array //
       targetedElement.innerHTML = clonedArray;
     }
   });
 }
 
+const capitalizeWords = function(input) {
+  // define an array that can be accessed within loop to combine strings into one //
+  let rejoinedArray = []
+  // create an array of the words in the string //
+  const wordsArray = input.split(" ");
+  // create loop to work with each value //
+  for (let i = 0; i <= wordsArray.length - 1; i++) {
+    // store word at current loop value in variable //
+    let item = wordsArray[i];
+    // create an array of the letters that make up that word //
+    let letters = item.split("");
+    // store first letter of array in variable and change to uppercase //
+    let firstChar = letters[0].toUpperCase();
+    // splice first letter in array, insert uppercase letter stored in variable //
+    letters.splice(0, 1, firstChar);
+    // join the letters back together, store in variable //
+    let rejoined = letters.join("");
+    // push to new array //
+    rejoinedArray.push(rejoined);
+  }
+  // join words into one string //
+  let finalResult = rejoinedArray.join(" ");
+  targetedElement = document.getElementById("capitalize-words");
+  targetedElement.innerHTML = finalResult;
+}
+
+reverseString("abra kadabra alakazam");
 removeFalsyWithoutMutating(myArray);
+capitalizeWords("these were some lower case words")
