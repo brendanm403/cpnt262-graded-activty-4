8960343c1f7fb7e95e54309b942236e22d33bcc3
Brendan Malycky <bmalycky@hotmail.com>
2023-10-23 13:17:08 -0600

    added function to remove falsy values

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const reverseString = function(input) {
+  // select element to render in //
   let targetedElement = document.getElementById("reversed-string");
   // get each individual letter into an array //
   let lettersArray = input.split("");
@@ -23,4 +24,44 @@ const reverseString = function(input) {
   }
 }
 
-reverseString("abra kadbra alakazam");
+reverseString("abra kadabra alakazam");
+
+const myArray = ["hello", 0, 5, 14, "", false, true];
+
+
+// This is the version that MUTATES THE ORIGINAL ARRAY //
+const removeFalsy = function (inputArr) {
+  // use map to loop through array and remove falsy values, replace with splice to show it was removed //
+  inputArr.map( (element, index, arr) => {
+    // statement that checks if the element is falsy //
+    if (!element) {
+      // use arr argument from map method to splice at the index of the falsy value, insert spliced //
+      arr.splice(index, 1, "spliced");
+      console.log("mapped array", arr);
+      console.log("original array", myArray);
+    }
+  });
+}
+
+// this is the version that DOES NOT MUTATE THE ORIGINAL ARRAY //
+const removeFalsyWithoutMutating = function (inputArr) {
+  // select element to render in //
+  let targetedElement = document.getElementById("remove-falsy-original");
+  // create clone of original array //
+  const clonedArray = inputArr.map( x => x);
+  // use map on the clone to avoid mutating original array //
+  clonedArray.map( (element, index) => {
+    //check if falsy value //
+    if (!element) {
+      // remove falsy values at its index, insert spliced //
+      clonedArray.splice(index, 1, "spliced");
+      // render original array //
+      targetedElement.innerHTML = myArray;
+      // render cloned array //
+      targetedElement = document.getElementById("remove-falsy-clone");
+      targetedElement.innerHTML = clonedArray;
+    }
+  });
+}
+
+removeFalsyWithoutMutating(myArray);
